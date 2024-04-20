dac4052cbc96b1101ccf11d3584d991287f92a87
Mouadh <ktarimouadh@gmail.com>
2024-04-20 23:04:00 +0100

    add visualization field generation, sight contact (failed)

--- Assets/Scripts/EnemyScripts/VisualizationField.cs
+++ Assets/Scripts/EnemyScripts/VisualizationField.cs
@@ -1,26 +1,95 @@
+/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
-public class VisualizationField: MonoBehaviour
-{
-    void Start()
-    {
-        Mesh mesh = new Mesh();
+public class FieldOfView : MonoBehaviour {
+
+    [SerializeField] private LayerMask layerMask;
+    private Mesh mesh;
+    private float fov;
+    private float viewDistance;
+    private Vector3 origin;
+    private float startingAngle;
+
+    private void Start() {
+        mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
+        fov = 90f;
+        viewDistance = 50f;
+        origin = Vector3.zero;
+    }
 
-        Vector3[] vertices = new Vector3[3];
-        Vector2[] uv = new Vector2[3];
-        int[] triangles = new int[3];
+    private void LateUpdate() {
+        int rayCount = 50;
+        float angle = startingAngle;
+        float angleIncrease = fov / rayCount;
 
-        vertices[0] = Vector3.zero;
-        vertices[1] = new Vector3(10, 0);
-        vertices[2] = new Vector3(0, -10);
-        
-        triangles[0] = 0;
-        triangles[1] = 1;
-        triangles[2] = 2;
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++) {
+            Vector3 vertex;
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(transform.parent.transform.position.normalized, UtilsClass.GetVectorFromAngle(transform.parent.transform.rotation.z).normalized, viewDistance, layerMask);
+            if (raycastHit2D.collider == null) {
+                // No hit
+                vertex = origin + UtilsClass.GetVectorFromAngle(angle) * viewDistance;
+            } else {
+                // Hit object
+                vertex = raycastHit2D.point;
+            }
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0) {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+
+            vertexIndex++;
+            angle -= angleIncrease;
+        }
+
 
         mesh.vertices = vertices;
         mesh.uv = uv;
         mesh.triangles = triangles;
+        mesh.bounds = new Bounds(origin, Vector3.one * 1000f);
     }
+
+    public void SetOrigin(Vector3 origin) {
+        this.origin = origin;
+    }
+
+    public void SetAimDirection(Vector3 aimDirection) {
+        startingAngle = UtilsClass.GetAngleFromVectorFloat(aimDirection) + fov / 2f;
+    }
+
+    public void SetFoV(float fov) {
+        this.fov = fov;
+    }
+
+    public void SetViewDistance(float viewDistance) {
+        this.viewDistance = viewDistance;
+    }
+
 }
